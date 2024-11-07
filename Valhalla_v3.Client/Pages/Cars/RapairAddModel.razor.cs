e51ed9e0f01430a9caf9f6b995e46a0c1a3d80ce
Mateusz <suszczewiczmati@gmail.com>
2024-11-07 21:48:45 +0100

    poprawki w pobieraniu i dodawniu mechaników

--- Valhalla_v3.Client/Pages/Cars/RapairAddModel.razor.cs
+++ Valhalla_v3.Client/Pages/Cars/RapairAddModel.razor.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.SignalR.Client;
 using Valhalla_v3.Shared.CarHistory;
 using static System.Net.WebRequestMethods;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace Valhalla_v3.Client.Pages.Cars;
 
@@ -13,7 +15,7 @@ public partial class RapairAddModel
 {
     private CarHistoryRepair formModel = new CarHistoryRepair();
     private List<Mechanic> ListMechanic = new();
-
+    private bool isMechanicOpen = false;
     [Parameter]
     public EventCallback<CarHistoryRepair> OnFormSubmit { get; set; }
 
@@ -45,8 +47,41 @@ public partial class RapairAddModel
         await OnFormSubmit.InvokeAsync(formModel);
     }
 
-    public async ValueTask DisposeAsync()
+
+    void OpenStation()
     {
-        await _hubConnection.DisposeAsync();
+        isMechanicOpen = true;
+        StateHasChanged();
+    }
+
+    async Task CloseStation()
+    {
+
+        isMechanicOpen = false;
+        StateHasChanged();
+    }
+
+    private async void HandleMechanicSubmit(Mechanic model)
+    {
+        //TODO: dodanie mechaników
+        model.OperatorCreateId = 3;
+        model.OperatorModifyId = 3;
+        var json = JsonSerializer.Serialize(model);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        try
+        {
+            var response = await Http.PostAsync(navigation.ToAbsoluteUri($"api/Mechanic"), content);
+            if (response.IsSuccessStatusCode)
+            {
+                LoadMechnic();
+                CloseStation();
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        CloseStation();
     }
 }
