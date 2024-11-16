86029cdd1e3f0adb9ec53869a655c240fbf7b60b
Mateusz <suszczewiczmati@gmail.com>
2024-11-16 20:27:57 +0100

    przebudowa widoków opłat

--- Valhalla_v3.Client/Pages/Fees/FeeList.razor.cs
+++ Valhalla_v3.Client/Pages/Fees/FeeList.razor.cs
@@ -1,9 +1,19 @@
-﻿namespace Valhalla_v3.Client.Pages.Fees;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Valhalla_v3.Client.Pages.Fees;
 
 public partial class FeeList
 {
+    [Parameter]
+    public int Id { get; set; }
     private Tabs activeTab = Tabs.currentMonth;
     
+
+    protected override async Task OnInitializedAsync()
+    {
+
+    }
+
     private void SelectTab(Tabs tab)
     {
         activeTab = tab;
