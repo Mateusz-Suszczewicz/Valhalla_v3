871c934b02b7451f3a3cf0a978a5bd3ea58b52b6
MateuszSuszczewicz <suszczewiczmati@gmail.com>
2024-12-20 14:28:20 +0100

    .

--- Valhalla_v3.Client/Helpers/CarHelpers.cs
+++ Valhalla_v3.Client/Helpers/CarHelpers.cs
@@ -38,6 +38,8 @@ public static class CarHelpers
                     litr += car.Fuels.Where(x => x.DateTimeModify.Month == 1 && x.DateTimeModify.Year == DateTime.Now.Year + 1).Select(z => z.Cost / z.CostPerLitr).FirstOrDefault();
                 var minMilage = car.Fuels?.Where(x => x.DateTimeModify.Month == i && x.DateTimeModify.Year == DateTime.Now.Year)?.Min(z => z.Mileage);
                 var maxMilage = car.Fuels?.Where(x => x.DateTimeModify.Month == i + 1 && x.DateTimeModify.Year == DateTime.Now.Year).Select(z => z.Mileage).FirstOrDefault();
+                if(maxMilage ==  null || maxMilage == 0)
+                    maxMilage = car.Fuels?.Where(x => x.DateTimeModify.Month == i && x.DateTimeModify.Year == DateTime.Now.Year).Max(z => z.Mileage);
                 var pastMilage = maxMilage - minMilage <= 0 ? 1 : maxMilage - minMilage;
                 var spalanie = Math.Round(litr / (Convert.ToDecimal(pastMilage) / 100),2);
                 liters.Add(Convert.ToDouble(spalanie));
